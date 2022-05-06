ddc357d9d8d82cef7b4b50ac519762527c9c90df
Kravchenkostepan2005 <101333763+Kravchenkostepan2005@users.noreply.github.com>
2022-05-06 10:18:57 +0200

    def.py

---
+++ def.py
@@ -0,0 +1,7 @@
+from main import add, subtract, multiply, divide
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+print(add(num1, num2))
+print(subtract(num1, num2))
+print(multiply(num1, num2))
+print(divide(num1, num2))
