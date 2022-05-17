050b90537a45e2e496af92fb11a8b68676f0b65b
Kravchenkostepan2005 <101333763+Kravchenkostepan2005@users.noreply.github.com>
2022-05-17 15:00:01 +0200

    calculator.py

---
+++ calculator.py
@@ -0,0 +1,56 @@
+def add(x: int, y: int) -> int:
+    "Function to add"
+    return x + y
+
+
+def subtract(x: int, y: int) -> int:
+    "Function to subtract"
+    return x - y
+
+
+def multiply(x: int, y: int) -> int:
+    "Function to multiply"
+    return x * y
+
+
+def divide(x: int, y: int) -> int:
+    "Function to divide"
+    return x / y
+
+
+print("Select operation: 1 - add, 2 - subtract, 3 - multiply, 4 - divide: ")
+
+
+while True:
+    choice = input("Enter choice(1/2/3/4): ")
+
+    if choice not in ("1", "2", "3", "4"):
+        print("invalid input")
+
+    else:
+        num1 = int(input("Enter first number: "))
+        num2 = int(input("Enter second number: "))
+
+    if choice == "1":
+        print(num1, "+", num2, "=", add(num1, num2))
+    elif choice == "2":
+        print(num1, "-", num2, "=", subtract(num1, num2))
+    elif choice == "3":
+        print(num1, "*", num2, "=", multiply(num1, num2))
+    elif choice == "4":
+
+        print(num1, "/", num2, "=", divide(num1, num2))
+        next_calculation = input("Let's do next calculation? (yes/no): ")
+        if next_calculation == "no":
+            break
+
+
+def check(num1, num2):
+    if num1.isdigit():
+        print(num1)
+    else:
+        print("mistake")
+    if num2.isdigit():
+        print(num2)
+    else:
+        print("mistake")
