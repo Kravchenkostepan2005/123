dc87ea285c743ab698f7cba56edb3b4d51c8e3dc
Kravchenkostepan2005 <101333763+Kravchenkostepan2005@users.noreply.github.com>
2022-06-03 09:59:52 +0200

    test_calc.py

---
+++ test_calc.py
@@ -0,0 +1,18 @@
+import unittest
+import lesson
+
+class Test(unittest.TestCase):
+    def test_sum(self):
+        self.assertEqual(lesson.add(4, 7), 11)
+
+    def test_sub(self):
+        self.assertEqual(lesson.subtract(4, 7), -3)
+
+    def test_mult(self):
+        self.assertEqual(lesson.multiply(4, 7), 28)
+
+    def test_div(self):
+        self.assertEqual(lesson.divide(15, 5), 3)
+
+if __name__ == "__main__":
+    unittest.main()
